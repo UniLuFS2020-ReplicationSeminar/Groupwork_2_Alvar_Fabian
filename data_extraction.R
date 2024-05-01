18001e421a06de86f6cdc38a5e55ba9d8790a44a
fabian666666 <fabian.kruegel@bluewin.ch>
2024-05-01 18:15:48 +0200

    fixed error so that the HTTP works properly now

--- data_extraction.R
+++ data_extraction.R
@@ -1,14 +1,23 @@
-# Example query parameters
-query_params <- list(
-  'q' = 'cryptocurrency OR Bitcoin OR Ethereum OR blockchain',  # Keywords for the search
-  'section' = 'technology',  # Focus on technology section for relevant articles
-  'from-date' = '2020-01-01',  
-  'to-date' = format(Sys.Date(), "%Y-%m-%d"),  # Up to today's date
-  'order-by' = 'relevance',  # Order by relevance 
-  'show-fields' = 'all',  # Fetch all fields (title, body, etc.)
-  'page-size' = 50,  # Number of results per page
-  'api-key' = api_key  # Use the API key
+library(rvest)
+library(jsonlite)
+library(httr)
+library(stringr)
+
+# Define the base URL for the Guardian API content endpoint
+url <- "https://content.guardianapis.com/search"
+
+# Setup query parameters
+articles <- list(
+  "q" = "cryptocurrency OR Bitcoin OR Ethereum OR blockchain",  # Keywords for the search
+  "from-date" = "2024-04-01",  # From 01.05.2014
+  "to-date" = format(Sys.Date(), "%Y-%m-%d"),  # Up to today's date
+  "order-by" = "newest",  # Order by publication date (newest first)
+  "show-fields" = "all",  # Fetch all fields (title, body, etc.)
+  "page-size" = 50,  # Number of results per page
+  "api-key" = api_key  # Use the API key
 )
 
 # Make the HTTP request
-response <- GET(url = base_url, query = query_params)
+response <- GET(url = url, query = articles)
+
+
--- set_up.R
+++ set_up.R
@@ -1,11 +1,10 @@
 
-
 # Read in and extract the API key from the csv file
 api_key_data <- read.csv("api_key.csv", header = FALSE)
 api_key <- api_key_data$V1[1] 
 
 # Set up the base URL for the content search endpoint
-base_url <- "https://content.guardianapis.com/search"
+url <- "https://content.guardianapis.com/search"
 
 
 
